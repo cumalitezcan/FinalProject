71ce686640aa7437e4cf97eb09100911517f872a
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-02-11 00:16:07 +0300

    addedUtilities

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleUI
         {
              ProductTest();
             // CategoryTest();
+
+            
         }
 
         private static void CategoryTest()
@@ -25,9 +27,18 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName+" / " + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
